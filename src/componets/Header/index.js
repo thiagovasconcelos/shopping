876ad5b32c58cd0d3fad236ed908305a6f1c4f65
Aluno <aluno@naverecife.local>
2022-08-22 21:45:19 -0300

    pasta Search

--- src/componets/Header/index.js
+++ src/componets/Header/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./style.css"
-import logo from "../../assets/logo_eco02.png"
+import logo from "../../assets/ecommerce001.png"
 export default function Header(props){
 return(
-    <div>
-        <header>
+    
+        <header className="Header">
         <img src={logo} alt="logo" />
         <h1>
             Rock Music Shopping 
         </h1>
       </header>
-    </div>
+   
 );
 
 }
