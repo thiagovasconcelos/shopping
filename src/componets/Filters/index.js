c5faa7c28734b3a0941d472ca05bb89d5a4a4213
Thiago vasconcelos <thiago.limavas@gmail.com>
2022-09-02 20:28:25 -0300

    Ingremento filters

--- src/componets/Filters/index.js
+++ src/componets/Filters/index.js
@@ -1,17 +1,26 @@
 import React from "react";
+import Produto from "../Produto";
 import "./style.css"
 export default function Filters(props){
+   const filter = Produto
+    
     return(
        
         <div className="sub">
-           <nav>
-            <a>Menu</a>
-            <a>Departamento</a>
-            <a>Quem somos</a>
+            <nav>
+                <a>Menu</a>
+                <a>Departamento</a>
+                <a>Quem somos</a>
             </nav>
-            <div className="filters">  
-                <button>Filtrar preço</button>
-                <button>Filtrar Categorias</button>
+            <div className="filters">   
+                <input placeholder="Menor preço" />
+                <input placeholder="Maior preço"/>
+                <button>Filtrar preço</button>                       
+            </div>
+            <div className="ordenar">
+                <p1>Ordenar: </p1>
+                <button>Maior</button>
+                <button>Menor</button>
             </div>
         </div>
                   
--- src/componets/Produto/index.js
+++ src/componets/Produto/index.js
@@ -6,10 +6,12 @@ export default function Produto(props){
 return(
     <div>
     <img className="prod" src="https://dummyjson.com/image/i/products/1/1.jpg" alt="iphone9"/>
+    <div className="txtProd">
     <h1>title Iphone 9</h1>
     <p>Aparelho cheio de tecnologia</p>
     <p>Preço: 2.000,00</p>
     <p>Avaliação 4,5</p>
+    </div>
     </div>
 );
 }
